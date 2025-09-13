744bf63ce624cdb8025fe52f90e862489e1085a4
Hieu Tran Ngoc <tranngochieu05102002@gmail.com>
2025-09-13 15:45:59 +0700

    update: Add dependencies for touch button

---
+++ source/src/bsp/bsp_touch_button.c
@@ -0,0 +1,28 @@
+neo-/**
+ * @file       bsp_touch_button.c
+ * @copyright  Copyright (C) 2025 NEO. All rights reserved.
+ * @license    This project is released under the NEO License.
+ * @version    1.0.0
+ * @date       2025-09
+ * @author     Hieu Tran Ngoc
+ * @brief      Board support package for touch button
+ * @note       None
+ */
+/* Includes ----------------------------------------------------------- */
+#include "bsp_touch_button.h"
+#include "iot_button.h"
+#include "touch_button.h"
+#include "touch_sensor_lowlevel.h"
+
+static const char *TAG = "BSP_TOUCH";
+
+/* Private defines ---------------------------------------------------- */
+/* Private enumerate/structure ---------------------------------------- */
+/* Private macros ----------------------------------------------------- */
+/* Public variables --------------------------------------------------- */
+/* Private variables -------------------------------------------------- */
+/* Private function prototypes ---------------------------------------- */
+/* Function definitions ----------------------------------------------- */
+
+
+/* End of file -------------------------------------------------------- */

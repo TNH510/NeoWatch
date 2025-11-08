424ab0e68f0541e18eb20d6f6cb8a337e8cc1adb
Nguyen Ba Vu Thach <thach30012@gmail.com>
2025-11-08 09:57:56 +0700

    update: Successfully add button task to run along with manager task

---
+++ source/inc/system/system_button.h
@@ -0,0 +1,27 @@
+/**
+ * @file       system_button.h
+ * @copyright  Copyright (C) 2025 NEO. All rights reserved.
+ * @license    This project is released under the NEO License.
+ * @version    1.0.0
+ * @date       2025-11
+ * @author     Thach Vu
+ * @brief      System Button Module
+ * @note       None
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __SYSTEM_BUTTON_H
+#define __SYSTEM_BUTTON_H
+
+/* Includes ----------------------------------------------------------- */
+/* Public defines ----------------------------------------------------- */
+/* Public enumerate/structure ----------------------------------------- */
+/* Public macros ------------------------------------------------------ */
+/* Public variables --------------------------------------------------- */
+/* Public function prototypes ----------------------------------------- */
+void system_button_init(void);
+
+/* -------------------------------------------------------------------- */
+#endif /* __SYSTEM_BUTTON_H */
+
+/* End of file -------------------------------------------------------- */

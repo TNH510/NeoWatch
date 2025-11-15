2d5e65a26d72852c0e2a954a6ee811a2e4915c86
Hieu Tran Ngoc <hieutranngoc@itrvn.com>
2025-11-15 09:52:59 +0700

    update: Add bsp general

---
+++ source/src/bsp/bsp.c
@@ -0,0 +1,32 @@
+/**
+ * @file       bsp.c
+ * @copyright  Copyright (C) 2025 NEO. All rights reserved.
+ * @license    This project is released under the NEO License.
+ * @version    1.0.0
+ * @date       2025-10
+ * @author     Hieu Tran Ngoc
+ * @brief      BSP General
+ * @note       None
+ */
+/* Includes ----------------------------------------------------------- */
+#include "bsp.h"
+#include "bsp_i2c.h"
+#include "bsp_rtc.h"
+#include "bsp_lcd.h"
+
+/* Private defines ---------------------------------------------------- */
+/* Private enumerate/structure ---------------------------------------- */
+/* Private macros ----------------------------------------------------- */
+/* Public variables --------------------------------------------------- */
+/* Private variables -------------------------------------------------- */
+/* Private function prototypes ---------------------------------------- */
+/* Function definitions ----------------------------------------------- */
+void bsp_init(void)
+{
+    // Initialize all BSP components
+    // bsp_i2c_init();
+    // bsp_lcd_init();
+    bsp_rtc_init();
+}
+
+/* End of file -------------------------------------------------------- */

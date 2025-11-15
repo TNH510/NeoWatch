dafcb28a2c0d93f1a417f2bd594651c6459b1cd1
Nguyen Ba Vu Thach <thach30012@gmail.com>
2025-11-15 09:25:23 +0700

    update: Restructure and create system button packages

--- source/src/bsp/bsp_button.c
+++ source/src/bsp/bsp_button.c
@@ -1,17 +1,15 @@
 /**
- * @file       system_button.c
+ * @file       bsp_button.c
  * @copyright  Copyright (C) 2025 NEO. All rights reserved.
  * @license    This project is released under the NEO License.
  * @version    1.0.0
- * @date       2025-10
- * @author     Thach Vu 
- * @brief      System Button Module
+ * @date       2025-11
+ * @author     Thach Vu
+ * @brief      BSP for button
  * @note       None
  */
 /* Includes ----------------------------------------------------------- */
-#include "system_button.h"
-#include "base_type.h"
-
+#include "bsp_button.h"
 
 /* Private defines ---------------------------------------------------- */
 /* Private enumerate/structure ---------------------------------------- */
@@ -19,23 +17,7 @@
 /* Public variables --------------------------------------------------- */
 /* Private variables -------------------------------------------------- */
 /* Private function prototypes ---------------------------------------- */
-static void system_button_task(void *pvParameters);
-
 /* Function definitions ----------------------------------------------- */
-void system_button_init(void)
-{
-    // Create a task for system button handling
-    xTaskCreate(system_button_task, "system_button_task", 2048, NULL, 10, NULL);
-}
 
-static void system_button_task(void *pvParameters)
-{
-    // Log hello world every second
-    while (1)
-    {
-        ESP_LOGI("SYS_BUTTON", "Hello World from System Button!");
 
-        vTaskDelay(pdMS_TO_TICKS(5000));
-    }
-}
 /* End of file -------------------------------------------------------- */
